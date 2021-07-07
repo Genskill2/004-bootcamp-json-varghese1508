794d5ad652da0028505057a5f9ea60d13bcadbcc
varghese1508 <55691245+varghese1508@users.noreply.github.com>
2021-07-07 12:11:17 +0530

    load_journal function done

--- correlation.py
+++ correlation.py
@@ -1,1 +1,16 @@
 # Add the functions in this file
+import json
+
+def load_journal(fname: str) -> dict:
+    f = open(fname,'r')     # opens file in write mode
+    data = json.load(f)     # loads json file and returns dict of values
+    return data
+
+def compute_phi():
+    pass
+
+def compute_correlations():
+    pass
+
+def diagnose():
+    pass
